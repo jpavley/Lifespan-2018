b651afef6957d69da55058336fb8a8b49219fa76
John Pavley <john.pavley@viacom.com>
2018-02-18 15:02:40 -0500

    recreated view controller code based on corrupted project

--- Lifespan 2018/ViewController.swift
+++ Lifespan 2018/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
+enum UserMode: String {
+    case day = "day"
+    case week = "week"
+    case month = "month"
+    case year = "year"
+}
+
 class ViewController: UIViewController {
 
+    // MARK:- Properties -
+    
+    var mode = UserMode.day
+    var name = "john"
+    
+    // MARK:- Outlets -
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // MARK:- Actions -
+    
+    @IBAction func optionsAction(_ sender: Any) {
+    }
+    
+    @IBAction func dayAction(_ sender: Any) {
+        mode = .day
+        updateUX()
+    }
+    
+    @IBAction func weekAction(_ sender: Any) {
+        mode = .week
+        updateUX()
+    }
+    
+    @IBAction func monthAction(_ sender: Any) {
+        mode = .month
+        updateUX()
+    }
+    
+    @IBAction func yearAction(_ sender: Any) {
+        mode = .year
+        updateUX()
+    }
+    
+    // MARK:- Methods -
+    
+    func updateUX() {
+        
+        titleLabel.text = "\(name)'s life in a \(mode.rawValue)".capitalized
+    }
+    
+    // MARK:- Overrides -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateUX()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
