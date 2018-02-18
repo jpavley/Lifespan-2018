0eefec972eba4cb47cc8b14986a683aea419bc3a
John Pavley <john.pavley@viacom.com>
2018-02-18 14:29:27 -0500

    Initial Commit

---
+++ Lifespan 2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lifespan 2018
+//
+//  Created by jpavley12 on 2/18/18.
+//  Copyright © 2018 jpavley12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
