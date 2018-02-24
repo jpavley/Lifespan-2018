f28a522a29f2ba22dfccf5340f01a214bef595ee
John Pavley <john@pavley.com>
2018-02-24 18:04:32 -0500

    day, week, month, year view controllers

---
+++ Lifespan 2018/YearViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YearViewController.swift
+//  Lifespan 2018
+//
+//  Created by jpavley12 on 2/24/18.
+//  Copyright © 2018 jpavley12. All rights reserved.
+//
+
+import UIKit
+
+class YearViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
