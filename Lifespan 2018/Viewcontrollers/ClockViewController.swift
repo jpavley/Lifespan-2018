6ca3b87d14811a94854c8d286bfae82fb2ddc7bd
John Pavley <john@pavley.com>
2018-03-24 23:06:38 -0400

    starting over with view controllers

--- Lifespan 2018/Viewcontrollers/ClockViewController.swift
+++ Lifespan 2018/Viewcontrollers/ClockViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DayViewController.swift
+//  ClockViewController.swift
 //  Lifespan 2018
 //
 //  Created by jpavley12 on 2/18/18.
@@ -14,9 +14,7 @@ class ClockViewController: UIViewController {
     @IBOutlet weak var timeSpanLabel: UILabel!
     @IBOutlet weak var agedLabel: UILabel!
     
-    weak var lifeSpan: Lifespan!
-    weak var lifeClock: LifeClock!
-    weak var userProfile: UserProfile!
+    var userProfile: UserProfile?
     
     let minuteHandTag = 200
     let hourHandTag = 300
@@ -39,54 +37,58 @@ class ClockViewController: UIViewController {
 
         let masterVC = parent as! MasterViewController
         userProfile = masterVC.userProfile
-        lifeSpan = masterVC.lifeSpan
-        lifeClock = masterVC.lifeClock
     }
     
     fileprivate func updateView() {
-        //updateLifeClock()
         
-        userNameLabel.text = userProfile.name
+        updateLifeClock()
         
-        let birthYear = String(format: "%.0f", userProfile.birthYear.setting)
-        let deathYear = String(format: "%.0f", CGFloat(userProfile.birthYear.setting) + lifeSpan.modifiedALE!)
+        userNameLabel.text = userProfile!.name
+        
+        let birthYear = String(format: "%.0f", userProfile!.birthYear.setting)
+        let deathYear = String(format: "%.0f", CGFloat(userProfile!.birthYear.setting) + lifeSpan!.modifiedALE!)
         timeSpanLabel.text = "\(birthYear) to \(deathYear)"
         
-        let age = String(format: "%.0f", lifeSpan.modifiedALE!)
+        let age = String(format: "%.0f", lifeSpan!.modifiedALE!)
         agedLabel.text = "Aged \(age)"
         
     }
     
     fileprivate func updateLifeClock() {
-        lifeClock = createLifeClockForUser()
-        setClock(with: lifeClock)
         
-        // DEBUG
-        // print("lifeClock.time \(lifeClock.time) for user \(userProfile.name)")
+        let ls = createLifespanForUser()
+        let lc = createLifeClockForUser(with: ls!)
+        setClock(with: lc)
         
-        if CalendarUtilities.stringToTime(timeString: "00:00:00") == lifeClock.time {
-            userProfile.livingOnBorrowedTime = true
+        if CalendarUtilities.stringToTime(timeString: "00:00:00") == lc.time {
+            userProfile!.livingOnBorrowedTime = true
         } else {
-            userProfile.livingOnBorrowedTime = false
+            userProfile!.livingOnBorrowedTime = false
         }
+        
     }
     
-    fileprivate func createLifeClockForUser() -> LifeClock {
+    fileprivate func createLifeClockForUser(with ls: Lifespan) -> LifeClock {
         
-        let birthDate = CalendarUtilities.stringToDate(dateString: userProfile.dob)
-        lifeSpan = Lifespan(name: userProfile.name, dateOfBirth: birthDate!, averageLifeExpectancy: userProfile.ale)
+        let spanTime = ls.lifespanAsTime()
+        let lc = LifeClock(time: spanTime!)
+        return lc
+    }
+    
+    fileprivate func createLifespanForUser() -> Lifespan? {
         
-        let activityLevelMod = SpanModifier(name: "activityLevel", value: CGFloat(userProfile.activityLevel.setting), positive: true)
-        lifeSpan.spanModifiers?.append(activityLevelMod)
+        let birthDate = CalendarUtilities.stringToDate(dateString: userProfile!.dob)
+        let ls = Lifespan(name: userProfile!.name, dateOfBirth: birthDate!, averageLifeExpectancy: userProfile!.ale)
         
-        let stressLevelMod = SpanModifier(name: "stressLevel", value: CGFloat(userProfile.stressLevel.setting), positive: false)
-        lifeSpan.spanModifiers?.append(stressLevelMod)
+        let activityLevelMod = SpanModifier(name: "activityLevel", value: CGFloat(userProfile!.activityLevel.setting), positive: true)
+        ls.spanModifiers?.append(activityLevelMod)
+        
+        let stressLevelMod = SpanModifier(name: "stressLevel", value: CGFloat(userProfile!.stressLevel.setting), positive: false)
+        ls.spanModifiers?.append(stressLevelMod)
+        
+        return ls
+    }
 
-        let spanTime = lifeSpan.lifespanAsTime()
-        let lifeClock = LifeClock(time: spanTime!)
-        return lifeClock
-    }
-    
     fileprivate func setClock(with lifeClock: LifeClock) {
         let hourHand = view.viewWithTag(hourHandTag) as! HourHandView
         let minuteHand = view.viewWithTag(minuteHandTag) as! MinuteHandView
@@ -103,22 +105,6 @@ class ClockViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        
-        if segue.destination is MasterViewController {
-            let destination = segue.destination as! MasterViewController
-            destination.userProfile = userProfile
-            destination.lifeSpan = lifeSpan
-            destination.lifeClock = lifeClock
-        }
-    }
-
 
 }
 
--- Lifespan 2018/Viewcontrollers/MasterViewController.swift
+++ Lifespan 2018/Viewcontrollers/MasterViewController.swift
@@ -16,8 +16,6 @@ import UIKit
 class MasterViewController: UIViewController {
     
     var userProfile: UserProfile?
-    var lifeSpan: Lifespan?
-    var lifeClock: LifeClock?
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
@@ -82,32 +80,7 @@ class MasterViewController: UIViewController {
         viewController.view.removeFromSuperview()
         viewController.removeFromParentViewController()
     }
-    
-    fileprivate func createLifeClockForUser(with ls: Lifespan) -> LifeClock {
         
-        let spanTime = ls.lifespanAsTime()
-        let lc = LifeClock(time: spanTime!)
-        return lc
-    }
-    
-    fileprivate func createLifespanForUser() -> Lifespan? {
-        
-        guard let userProfile = userProfile  else {
-            return nil
-        }
-        
-        let birthDate = CalendarUtilities.stringToDate(dateString: userProfile.dob)
-        let ls = Lifespan(name: userProfile.name, dateOfBirth: birthDate!, averageLifeExpectancy: userProfile.ale)
-        
-        let activityLevelMod = SpanModifier(name: "activityLevel", value: CGFloat(userProfile.activityLevel.setting), positive: true)
-        ls.spanModifiers?.append(activityLevelMod)
-        
-        let stressLevelMod = SpanModifier(name: "stressLevel", value: CGFloat(userProfile.stressLevel.setting), positive: false)
-        ls.spanModifiers?.append(stressLevelMod)
-
-        return ls
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -115,8 +88,6 @@ class MasterViewController: UIViewController {
         
         if userProfile == nil {
             userProfile = UserProfile()
-            lifeSpan = createLifespanForUser()
-            lifeClock = createLifeClockForUser(with: lifeSpan!)
         }
         
         currentSegmentID = clockSegmentID
@@ -182,21 +153,5 @@ class MasterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        
-        if segue.destination is ClockViewController {
-            let destination = segue.destination as! ClockViewController
-            destination.userProfile = userProfile
-            destination.lifeSpan = lifeSpan
-            destination.lifeClock = lifeClock
-        }
-    }
-    */
 
 }
